607bba700782de48e87665c4396da1446a9423cd
fed-gren <deveb22@gmail.com>
2019-09-26 01:17:23 +0900

    feat: export KakaoMapContext

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import { KakaoMapContext } from "./MapLib/KakaoMap";
+export default KakaoMapContext;
+
 export { default as KakaoMap } from "./MapLib/KakaoMap";
 export { default as Marker } from "./MapLib/Marker";
 export { default as CustomOverlay } from "./MapLib/CustomOverlay";
