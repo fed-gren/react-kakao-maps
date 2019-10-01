25ada91d4476ec782ec7b0f3baf3e5d7e21caf9e
fed-gren <deveb22@gmail.com>
2019-10-01 16:23:02 +0900

    env: add eslint plugins for react-hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,10 @@ module.exports = {
   },
   'plugins': [
     'react',
+    'react-hooks'
   ],
   'rules': {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
 };
